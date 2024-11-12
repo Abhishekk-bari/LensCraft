31140d3f44887a0ad80d1bb06fb6d0279799c480
Abhi <abhi2bari929@gmail.com>
2024-11-12 10:06:40 +0530

    Navbar with styling

--- src/App.jsx
+++ src/App.jsx
@@ -12,12 +12,32 @@ function App() {
 
   return (
     <>
-    <div className="w-full relative min-h-screen">
+    <div className="w-full bg-black relative min-h-screen font-['Helvetica_Now_Display']">
       {data[0].map((canvasdets, index) => (
         <Canvas details={canvasdets} />
       ))}
+
+<div className='w-full h-screen text-white'>
+      <nav className='fixed top-0 left-0 w-full p-8 flex justify-between z-50'>
+        <div className='brand text-2xl font-regular'>TS</div>
+        <div className='links flex gap-10'>
+          {["Home", "About", "Projects", "Contact"] .map((link, index) => (
+            <a key={index}
+            href={`#${link.toLowerCase()}`}
+            className='text-md hover:text-gray-300'
+            >
+              {link}
+            </a>
+          ))}
+        </div>
+      </nav>
+
+      
     </div>
-    <div className="w-full relative min-h-screen">
+    </div> 
+
+    
+    {/* <div className="w-full relative min-h-screen">
       {data[1].map((canvasdets, index) => (
         <Canvas details={canvasdets} />
       ))}
@@ -31,7 +51,7 @@ function App() {
       {data[3].map((canvasdets, index) => (
         <Canvas details={canvasdets} />
       ))}
-    </div>
+    </div> */}
     </>
   )
 }
