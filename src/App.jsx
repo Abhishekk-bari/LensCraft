4e604bee89532678365626b277e5a444c8af57d4
Abhi <abhi2bari929@gmail.com>
2024-11-23 12:10:44 +0530

    mobile responsive

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,9 @@ import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Testimonial from "./components/pages/Testimonial";
+// import Testimonial from "./components/pages/Testimonial"; // under working don't touch 
 import Home from "./components/pages/Home";
-import About from "./components/pages/About";
+// import About from "./components/pages/About";              // under working don't touch 
 
 function App() {
   const [showCanvas, setShowCanvas] = useState(false);
@@ -79,8 +79,8 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home showCanvas={showCanvas} setShowCanvas={setShowCanvas} headingref={headingref} growingSpan={growingSpan} />} />
-        <Route path="/testimonial" element={<Testimonial />} />
-        <Route path="/about" element={<About />} />
+        {/* <Route path="/testimonial" element={<Testimonial />} /> */}
+        {/* <Route path="/about" element={<About />} /> */}
       </Routes>
     </Router>
   );
